93feb20344b5a8132ecbaf8ec871c45cfa48a460
Jaren Lo <qnurye@gmail.com>
2026-03-23 04:16:27 +0800

    chore(home): set username and home directory

--- nix/modules/home/default.nix
+++ nix/modules/home/default.nix
@@ -6,6 +6,8 @@
   ];
 
   # Basic home-manager configuration
+  home.username = "qnurye";
+  home.homeDirectory = "/Users/qnurye";
   home.stateVersion = "24.05";
   programs.home-manager.enable = true;
 }
